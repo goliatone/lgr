de112a8a2dc6f262326f896b323ed116af7aa0b8
goliatone <140582+goliatone@users.noreply.github.com>
2023-01-15 11:16:13 -0800

    add: update version

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -1,7 +1,18 @@
 package version
 
 import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/andreyvit/diff"
+)
+
+var (
+	create = false
+	update = false
 )
 
 func Test_GetVersion(t *testing.T) {
@@ -19,4 +30,64 @@ func Test_GetVersion(t *testing.T) {
 	if result != expected {
 		t.Errorf("GetVersion failed, expected %v got %v", expected, result)
 	}
+}
+
+func Test_Print(t *testing.T) {
+
+	stdout := new(bytes.Buffer)
+
+	Tag = "0.0.17"
+	Time = "2023-01-15T00:10:48Z"
+	User = "goliatone"
+	Commit = "61bcee6416c00663781f36fefda77c4683809453"
+
+	actual := stdout.Bytes()
+	expected := LoadFixture(t, "version.txt", actual)
+
+	if err := Print(stdout); err != nil {
+		t.Fatalf("unwanted error: %v", err)
+	}
+
+	if res := string(expected) == string(stdout.String()); !res {
+		d := diff.LineDiff(string(expected), string(actual))
+		t.Errorf("output not as expected:\n%v", d)
+	}
+}
+
+func LoadFixture(t *testing.T, name string, actual []byte) []byte {
+	if update {
+		UpdateFixture(t, name, actual)
+	}
+	g, err := _loadFixtureFile(name)
+	if err != nil {
+		if _, ok := err.(*os.PathError); !ok || !create {
+			t.Fatalf("error reading golden file %s: %v", name, err)
+		}
+		CreateFixture(t, name)
+		LoadFixture(t, name, actual)
+	}
+	return g
+}
+
+func CreateFixture(t *testing.T, name string) {
+	path := filepath.Join("../../testdata", name)
+	f, err := os.Create(path)
+	if err != nil {
+		t.Fatalf("error creating golden file %s: %v", name, err)
+	}
+	if err = f.Close(); err != nil {
+		t.Fatalf("error closing golden file %s: %v", name, err)
+	}
+}
+
+func UpdateFixture(t *testing.T, name string, actual []byte) {
+	path := filepath.Join("../../testdata", name)
+	if err := ioutil.WriteFile(path, actual, 0644); err != nil {
+		t.Fatalf("error updating golden file %s: %v", name, err)
+	}
+}
+
+func _loadFixtureFile(name string) ([]byte, error) {
+	path := filepath.Join("../../testdata", name)
+	return ioutil.ReadFile(path)
 }
